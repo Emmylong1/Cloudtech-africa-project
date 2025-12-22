b92140254e0d2afdcb9c0d0b3a5d3286b680bd38
Ibok Emmanuel <98855013+Emmylong1@users.noreply.github.com>
2025-12-23 00:39:45 +0100

    Update main.tf

--- my-tf-script/main.tf
+++ my-tf-script/main.tf
@@ -14,21 +14,22 @@ resource "local_file" "kubeconfig" {
 }
 
 module "gcp-network" {
-  source       = "terraform-google-modules/network/google"
-  version      = "6.0.0"
+  source  = "terraform-google-modules/network/google"
+  version = "6.0.0"
+
   project_id   = var.project_id
-  network_name = "${var.network}-${var.env_name}"
+  network_name = "${var.network_name}-${var.env_name}"
 
   subnets = [
     {
-      subnet_name   = "${var.subnetwork}-${var.env_name}"
+      subnet_name   = "${var.subnetwork_name}-${var.env_name}"
       subnet_ip     = "10.10.0.0/16"
       subnet_region = var.region
-    },
+    }
   ]
 
   secondary_ranges = {
-    "${var.subnetwork}-${var.env_name}" = [
+    "${var.subnetwork_name}-${var.env_name}" = [
       {
         range_name    = var.ip_range_pods_name
         ip_cidr_range = "10.20.0.0/16"
@@ -36,10 +37,11 @@ module "gcp-network" {
       {
         range_name    = var.ip_range_services_name
         ip_cidr_range = "10.30.0.0/16"
-      },
+      }
     ]
   }
 }
+
 
 data "google_client_config" "default" {}
 
@@ -72,3 +74,4 @@ module "gke" {
     },
   ]
 }
+
